6e1745f914b581d45629bb73865497e91cd6f37c
jamesariess <jamesariesconcepcion@gmail.com>
2026-04-26 19:09:49 +0800

    adding connection

---
+++ backend/conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = 'Localhost';
+$username = 'root';
+$password = '';
+$dbname = 'spendtrackfinance';
+
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+
+?>
